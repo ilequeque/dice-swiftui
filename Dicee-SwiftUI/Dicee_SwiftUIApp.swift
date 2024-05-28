d3abc30ac4511f11acc2fe6728d48ca909ad90fd
ilequeque <ilyas.shakhabdinov@gmail.com>
2024-05-28 12:05:11 +0500

    dice swiftui

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Iliyas Shakhabdin on 25.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
